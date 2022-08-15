07755d56ac7950d5db28d0d8ebd9d7d39bed37fc
baloqsmash <111299109+baloqsmash@users.noreply.github.com>
2022-08-16 01:29:58 +0200

    Update minting.js

--- Mint/minting.js
+++ Mint/minting.js
@@ -108,7 +108,7 @@ async function mint(numToMint){
 	if(!await isConnected())
 		return;
 		
-	const pricePerToken = 0.005;
+	const pricePerToken = 0.0069;
 	const priceForMint = pricePerToken * numToMint;
 	var mintPrice = web3.utils.toWei(String(priceForMint), 'ether');
 	try {
